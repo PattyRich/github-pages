02be15853f083cb178ebe4a07ac6a6b81200cdb6
Patrick Richardson <patrickr@crossbrowsertesting.com>
2021-11-17 17:25:56 -0600

    Updates

--- static/js/2.6bd2993f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgithub-pages"]=this["webpackJsonpgithub-pages"]||[]).push([[2],{19:function(a,e,t){"use strict";t.r(e),t.d(e,"data",(function(){return r}));var r={name:"tob",chance:2.75,items:[{name:"Scythe of vitur (uncharged)",rate:.145},{name:"Justiciar legguards",rate:.29},{name:"Justiciar chestguard",rate:.29},{name:"Justiciar faceguard",rate:.29},{name:"Sanguinesti staff (uncharged)",rate:.29},{name:"Ghrazi rapier",rate:.29},{name:"Avernic defender hilt",rate:1.16}],pet:{rate:650,name:"Lil' zik"}}}}]);
-//# sourceMappingURL=2.6bd2993f.chunk.js.map
