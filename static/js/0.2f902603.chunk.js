ed582c147c5c9f4d9da6b922594d03c3e90a7259
Patrick Richardson <patrickr@crossbrowsertesting.com>
2021-11-19 19:05:14 -0600

    Updates

--- static/js/0.2f902603.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgithub-pages"]=this["webpackJsonpgithub-pages"]||[]).push([[0],{21:function(e,a,t){"use strict";t.r(a),t.d(a,"data",(function(){return n}));var n={name:"cg",chance:4.25,items:[{name:"Crystal weapon seed",rate:8},{name:"Crystal armour seed",rate:8},{name:"Enhanced crystal weapon seed",rate:1}],pet:{rate:800,name:"Youngllef"}}}}]);
-//# sourceMappingURL=0.2f902603.chunk.js.map
---
+++ static/js/0.8dbcd5ca.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgithub-pages"]=this["webpackJsonpgithub-pages"]||[]).push([[0],{22:function(e,a,t){"use strict";t.r(a),t.d(a,"data",(function(){return n}));var n={name:"cg",chance:4.25,items:[{name:"Crystal weapon seed",rate:8},{name:"Crystal armour seed",rate:8},{name:"Enhanced crystal weapon seed",rate:1}],pet:{rate:800,name:"Youngllef"}}}}]);
+//# sourceMappingURL=0.8dbcd5ca.chunk.js.map
