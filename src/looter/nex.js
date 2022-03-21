558fdba9bdfe2d08d12f4e15cf023e07b1f21860
Patrick <patrickrich95@gmail.com>
2022-03-21 12:56:49 -0500

    nex

---
+++ src/looter/nex.js
@@ -0,0 +1,34 @@
+export let data = {
+	name: 'nex',
+	items: [
+		{
+			name: 'Zaryte vambraces',
+			rate: 1/212
+		},
+		{
+			name: 'Nihil horn',
+			rate: 1/318
+		},
+		{
+			name: 'Torva full helm (damaged)',
+			rate: 1/318
+		},
+		{
+			name: 'Torva platebody (damaged)',
+			rate: 1/318
+		},
+		{
+			name: 'Torva platelegs (damaged)',
+			rate: 1/318
+		},
+		{
+			name: 'Ancient hilt',
+			rate: 1/636 
+		}
+	],
+	pet:{
+		rate: 500,
+		name: "Nexling"
+	} 
+}
+
