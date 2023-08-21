d5037aede774766704e6f4718c422a5780798190
Patrick <patrickrich95@gmail.com>
2023-08-20 20:18:19 -0500

    Updates

---
+++ static/js/62.9e3fade5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_pages=self.webpackChunkgithub_pages||[]).push([[62],{9062:function(e,t,a){a.r(t),a.d(t,{data:function(){return r}});var r={name:"whisperer",items:[{name:"Bellator vestige",rate:3/512},{name:"Siren's staff",rate:1/512},{name:"Chromium ingot",rate:3/512},{name:"Virtus mask",rate:1/1536},{name:"Virtus robe top",rate:1/1536},{name:"Virtus robe bottom",rate:1/1536}],pet:{rate:2e3,name:"Wisp"}}}}]);
+//# sourceMappingURL=62.9e3fade5.chunk.js.map
