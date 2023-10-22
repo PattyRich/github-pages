5caff3507df65080f67c0da7c9a27d4c90ebe2cb
Patrick <patrickrich95@gmail.com>
2023-10-21 23:02:52 -0500

    Updates

---
+++ static/js/305.fcddf24a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_pages=self.webpackChunkgithub_pages||[]).push([[305],{6305:function(e,a,t){t.r(a),t.d(a,{data:function(){return r}});var r={name:"leviathan",items:[{name:"Venator vestige",rate:3/768},{name:"Leviathan's lure",rate:1/768},{name:"Chromium ingot",rate:3/768},{name:"Virtus mask",rate:1/2304,extra:!0},{name:"Virtus robe top",rate:1/2304,extra:!0},{name:"Virtus robe bottom",rate:1/2304,extra:!0}],pet:{rate:2500,name:"Lil'viathan"}}}}]);
+//# sourceMappingURL=305.fcddf24a.chunk.js.map
