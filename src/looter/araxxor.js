6d5f3e02460fb461ba3f7e73f54993926e59169d
Patrick Richardson <patrickrich95@gmail.com>
2026-03-09 19:04:53 -0500

    Update pet data and add new looter bosses

---
+++ src/looter/araxxor.js
@@ -0,0 +1,29 @@
+export let data = {
+	name: 'araxxor',
+	items: [
+		{
+			name: 'Araxyte fang',
+			rate: 1/600
+		},
+		{
+			name: 'Noxious pommel',
+			rate: 1/600,
+			sequence: 1
+		},
+		{
+			name: 'Noxious point',
+			rate: 1/600,
+			sequence: 2
+		},
+		{
+			name: 'Noxious blade',
+			rate: 1/600,
+			sequence: 3
+		}
+	],
+	pet:{
+		rate: 3000,
+		name: "Nid"
+	} 
+}
+
