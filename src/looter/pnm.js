af9c794a7a50d32e507485893708b043c5c45bdb
Patrick Richardson <patrickrich95@gmail.com>
2026-04-22 13:25:01 -0500

    unit tests + pnm rates

--- src/looter/pnm.js
+++ src/looter/pnm.js
@@ -3,40 +3,40 @@ export let data = {
 	items: [
 		{
 			name: 'Nightmare staff',
-			rate: 1/533
+			rate: 1 / 507.2
 		},
 		{
 			name: 'Inquisitor\'s great helm',
-			rate: 1/700
+			rate: 1 / 700
 		},
 		{
 			name: 'Inquisitor\'s hauberk',
-			rate: 1/700
+			rate: 1 / 700
 		},
 		{
 			name: 'Inquisitor\'s plateskirt',
-			rate: 1/700
+			rate: 1 / 700
 		},
 		{
 			name: 'Inquisitor\'s mace',
-			rate: 1/1250
+			rate: 1 / 1129
 		},
 		{
 			name: 'Eldritch orb',
-			rate: 1/1600
+			rate: 1 / 1600
 		},
 		{
 			name: 'Harmonised orb',
-			rate: 1/1600
+			rate: 1 / 1600
 		},
 		{
 			name: 'Volatile orb',
-			rate: 1/1600
+			rate: 1 / 1600
 		}
 	],
-	pet:{
+	pet: {
 		rate: 1400,
 		name: "Little nightmare"
-	} 
+	}
 }
 
