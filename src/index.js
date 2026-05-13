57e07ebcb392e6a8d45b45a8d0acda4c3bb4c5f2
Patrick Richardson <patrickrich95@gmail.com>
2026-05-12 22:18:37 -0500

    back live

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ if (darkMode === 'true' || darkMode === true) {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const IS_MAINTENANCE = true;
+const IS_MAINTENANCE = false;
 
 const Maintenance = (
 	<main style={{
